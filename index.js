af7498b55c96f4bc529642624721f7e96c31c7d1
Suchita Ghadge <36835240+SuchitaGhadge@users.noreply.github.com>
2021-09-13 17:53:11 +0530

    updated questions

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var readLineSync = require('readline-sync');
 var score = 0;
-var userName = readLineSync.question("What's your name? " )
+var userName = readLineSync.question("What's your name? ")
 console.log("Welcome " + userName + " to Do you know Suchita?")
 
 // play function
 
-function play(question, answer){
+function play(question, answer) {
   var userAnswer = readLineSync.question(question);
 
 
-  if (userAnswer.toLowerCase() === answer.toLowerCase()){
+  if (userAnswer.toLowerCase() === answer.toLowerCase()) {
     console.log("You're right!")
-    score = score+1;
-  }else{
+    score = score + 1;
+  } else {
     console.log("Wrong answer!")
   }
   console.log("----------------------------");
@@ -23,20 +23,28 @@ var questions = [{
   answer: "Mumbai"
 },
 {
-  question: "What's my favorite dish? ",
-  answer: "Gulab Jamun"
+  question: "Which is my native place? ",
+  answer: "Mahabaleshwar"
 },
 {
   question: "What's my favorite color? ",
   answer: "White"
-}]
+},
+{
+  question: "My favorite place in Mumbai ",
+  answer: "Marine lines"
+},
+{
+  question: "What is my date of birth (dd/mm/yyyy)",
+  answer: "19/12/1993"
+},]
 
-for(i=0; i<questions.length; i++){
+for (i = 0; i < questions.length; i++) {
   var currentQuestion = questions[i];
   play(currentQuestion.question, currentQuestion.answer)
 }
 
 console.log("Yay! your score is: ", score)
-if(score>=2){
+if (score >= 2) {
   console.log(userName, score)
 }
